34c49c26a7a4c48ab43a873cbcd4b6977632b383
piccionipedro <pnicopiccioni@gmail.com>
2022-11-23 22:01:50 -0300

    PrimeraEntrega

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Navbar() {
+  return ( 
+    <>
+        <NavContainer>
+            <h2>Escuela de <span> Peluquería </span> </h2>
+            <div>
+                <a href="">Historia</a>
+                <a href="">Productos</a>
+                <a href="">Carreras</a>
+                <a href="">Contacto</a>
+            </div>
+        </NavContainer>
+    </>
+  )
+}
+
+export default Navbar
+
+const NavContainer = styled.nav`
+
+    h2{
+        color: wheat;
+        font-weight: bold;
+    }
+    padding: .4rem;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a{
+        color: wheat;
+        text-decoration: none;
+        margin-right: 1rem;
+    }
+    a:hover{
+        font-weight: bold;
+        color: white;
+    }
+
+
+
+`
